5a9603ca39c120c6516e312e3ef49f7649abfd8d
Joseph Moore <josephpmoore@protonmail.com>
2022-05-30 14:56:47 +0800

    Add entries and embedding method

---
+++ Metadata.py
@@ -0,0 +1,14 @@
+import pandas as pd
+new = open("Entries.html", "w", encoding="utf8")
+df = pd.read_excel("Entry_Metadata.xlsx")
+link = df["Link"]
+title = df["Title"]
+eng = df["English"]
+chn = df["Chinese"]
+aut = df["Author"]
+for i in range(0, len(df.index)):
+    hyperlink = "<a target=_blank, href=\"%s\">%s</a>" % (link[i], title[i])
+    description = "<p>%s / %s</p>" % (eng[i], chn[i])
+    author = "<p class=\"Author\">By %s</p>" % (aut[i])
+    lines = "<div class=\"Entry\">\n    %s\n    %s\n    %s\n</div>\n" % (hyperlink, description, author)
+    new.write(lines)
