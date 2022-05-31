3842919366c74c6044f726e59fae4f86ce7738f2
Joseph Moore <josephpmoore@protonmail.com>
2022-05-31 17:11:42 +0800

    Restructure entries

--- Metadata.py
+++ Metadata.py
@@ -7,8 +7,8 @@ eng = df["English"]
 chn = df["Chinese"]
 aut = df["Author"]
 for i in range(0, len(df.index)):
-    hyperlink = "<a target=_blank, href=\"%s\">%s</a>" % (link[i], title[i])
-    description = "<p>%s / %s</p>" % (eng[i], chn[i])
-    author = "<p class=\"Author\">By %s</p>" % (aut[i])
-    lines = "<div class=\"Entry\">\n    %s\n    %s\n    %s\n</div>\n" % (hyperlink, description, author)
+    hyperlink = "<a class=\"entry-hyperlink\" href=\"%s\" target=_blank>%s</a>" % (link[i], title[i])
+    description = "<p class=\"entry-description\">%s / %s</p>" % (eng[i], chn[i])
+    author = "<p class=\"entry-author\">By %s</p>" % (aut[i])
+    lines = "<div class=\"entry\">\n    %s\n    %s\n    %s\n</div>\n" % (hyperlink, description, author)
     new.write(lines)
